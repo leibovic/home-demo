8d5de952a28b0059088f3b120f852e874ea1a22f
Margaret Leibovic <margaret.leibovic@gmail.com>
2013-09-10 15:51:33 -0700

    initial commit

---
+++ bootstrap.js
@@ -0,0 +1,79 @@
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+
+Cu.import("resource://gre/modules/Home.jsm");
+
+// Keep track of the message id so that we can remove it.
+var gMessageId;
+
+function loadIntoWindow(window) {
+  gMessageId = Home.banner.add({
+    text: "Follow @FennecNightly on Twitter!",
+    icon: "chrome://homedemo/skin/fennecIcon.png",
+    onclick: function() {
+      window.BrowserApp.addTab("https://twitter.com/FennecNightly");
+    }
+  });
+}
+
+function unloadFromWindow(window) {
+  Home.banner.remove(gMessageId);
+}
+
+/**
+ * bootstrap.js API
+ */
+var windowListener = {
+  onOpenWindow: function(aWindow) {
+    // Wait for the window to finish loading
+    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+    domWindow.addEventListener("load", function() {
+      domWindow.removeEventListener("load", arguments.callee, false);
+      loadIntoWindow(domWindow);
+    }, false);
+  },
+  
+  onCloseWindow: function(aWindow) {
+  },
+  
+  onWindowTitleChange: function(aWindow, aTitle) {
+  }
+};
+
+function startup(aData, aReason) {
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+
+  // Load into any existing windows
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    loadIntoWindow(domWindow);
+  }
+
+  // Load into any new windows
+  wm.addListener(windowListener);
+}
+
+function shutdown(aData, aReason) {
+  // When the application is shutting down we normally don't have to clean
+  // up any UI changes made
+  if (aReason == APP_SHUTDOWN)
+    return;
+
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+
+  // Stop listening for new windows
+  wm.removeListener(windowListener);
+
+  // Unload from any existing windows
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    unloadFromWindow(domWindow);
+  }
+}
+
+function install(aData, aReason) {
+}
+
+function uninstall(aData, aReason) {
+}
